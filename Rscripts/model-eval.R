472075590b849f46936edfb0804b6f6d0495255c
rashe877 <rashe877@student.liu.se>
2018-05-07 09:03:34 +0200

    Graphs and cool accidental filter
    Add graphs with error metrics s.a. color, geometric and luminance.
    Also, a cool filter for images was accidentally found giving a cool glow
    effect!

--- Rscripts/model-eval.R
+++ Rscripts/model-eval.R
@@ -60,14 +60,26 @@ plot_tri_geom_tex_error <- function(data) {
 
 
 # Plot triangles vs error
-plot_error <- function(data, y) {
-  ggplot(data[as.character(data$Detail) != " base",], 
-         aes_string("Triangles", y, color="Reducer", shape="Model")) +
+plot_error <- function(data, error) {
+  ggplot(data[as.character(data$Detail) != "base",], 
+         aes_string("Triangles", error, color="Reducer", shape="Model")) +
     geom_point(size=3) +
     geom_line() +
     scale_x_log10() +
     scale_y_log10() +
     annotation_logticks()
+}
+
+plot_error_labeled <- function(data, error, xlabel, ylabel) {
+  ggplot(data[as.character(data$Detail) != "base",], 
+         aes_string("Triangles", error, color="Model", shape="Reducer")) +
+    geom_point(size=3) +
+    geom_line() +
+    scale_x_log10() +
+    scale_y_log10() +
+    annotation_logticks() +
+    xlab(xlabel) +
+    ylab(ylabel)
 }
 
 
@@ -82,40 +94,46 @@ create_header <- function(names) {
   return(setNames(data.frame(matrix(ncol=length(names), nrow=0)),names))
 }
 
-data1 <- importData("data1.csv")
-data <- importData("data.csv")
-data_tex <- importData("data-tex.csv")
-data_notex <- importData("data-notex.csv")
-data_tex_bound <- importData("data-tex-bound.csv")
+#######################
+mk2_luminance_tex <- importData("mk2-luminance-novol-nobound.csv")
+mk2_luminance_tex_bound <- importData("mk2-luminance-novol-bound.csv")
+mk2_luminance_tex_vol <- importData("Backup/mk2-luminance-vol-nobound.csv")
+mk2_luminance_tex_vol_bound <- importData("Backup/mk2-luminance-vol-bound.csv")
 
-mk_distance <- importData("mk-distance.csv")
-mk2_distance_tex <- importData("mk2-distance-nobound.csv")
-mk2_distance_tex_bound <- importData("mk2-distance-bound.csv")
-mk2_distance_tex$Reducer <- rep("mk2 - texture, NO bound", nrow(mk2_distance_tex))
-mk2_distance_tex_bound$Reducer <- rep("mk2 - texture, bound", nrow(mk2_distance_tex_bound))
+mk2_luminance_tex$Reducer <- rep("texture", nrow(mk2_luminance_novol_tex))
+mk2_luminance_tex_bound$Reducer <- rep("texture, seam", nrow(mk2_luminance_novol_tex))
+mk2_luminance_tex_vol$Reducer <- rep("texture, volume", nrow(mk2_luminance_novol_tex))
+mk2_luminance_tex_vol_bound$Reducer <- rep("texture, volume, seam", nrow(mk2_luminance_novol_tex))
+#######################
 
-mk2_luminance_novol_tex <- importData("mk2-luminance-novol-nobound.csv")
-mk2_luminance_novol_tex_bound <- importData("mk2-luminance-novol-bound.csv")
-mk2_luminance_vol_tex <- importData("Backup/mk2-luminance-vol-nobound.csv")
-mk2_luminance_vol_tex_bound <- importData("Backup/mk2-luminance-vol-bound.csv")
-mk2_luminance_volA_tex <- importData("mk2-luminance-vol-nobound.csv")     #### Area weighted
-mk2_luminance_volA_tex_bound <- importData("mk2-luminance-vol-bound.csv") #### Area weighted
-mk2_luminance_novol_tex$Reducer <- rep("mk2 - texture, NO vol, NO bound", nrow(mk2_luminance_novol_tex))
-mk2_luminance_novol_tex_bound$Reducer <- rep("mk2 - texture, NO vol, bound", nrow(mk2_luminance_novol_tex_bound))
-mk2_luminance_vol_tex$Reducer <- rep("mk2 - texture, vol, NO bound", nrow(mk2_luminance_vol_tex))
-mk2_luminance_vol_tex_bound$Reducer <- rep("mk2 - texture, vol, bound", nrow(mk2_luminance_vol_tex_bound))
-mk2_luminance_volA_tex$Reducer <- rep("mk2 - texture, volA, NO bound", nrow(mk2_luminance_volA_tex))
-mk2_luminance_volA_tex_bound$Reducer <- rep("mk2 - texture, volA, bound", nrow(mk2_luminance_volA_tex_bound))
-
-mk2_hausdorff_volA_tex <- importData("mk2-hausdorff-volarea-nobound.csv")
-mk2_hausdorff_volA_tex_bound <- importData("mk2-hausdorff-volarea-bound.csv")
-mk2_hausdorff_volA_tex$Reducer <- rep("mk2 - texture, volA, NO bound", nrow(mk2_hausdorff_volA_tex))
-mk2_hausdorff_volA_tex_bound$Reducer <- rep("mk2 - texture, volA, bound", nrow(mk2_hausdorff_volA_tex_bound))
+mk2_luminance_tex$Reducer <- rep("mk2 - texture", nrow(mk2_luminance_tex))
+mk2_luminance_novol_tex_bound$Reducer <- rep("mk2 - texture, seam", nrow(mk2_luminance_tex_bound))
+mk2_luminance_vol_tex$Reducer <- rep("mk2 - texture, vol", nrow(mk2_luminance_votex))
+mk2_luminance_vol_tex_bound$Reducer <- rep("mk2 - texture, vol, seam", nrow(mk2_luminance_vol_tex_bound))
+mk2_luminance_tex <- importData("Backup/mk2-luminance-novol-nobound.csv")
+mk2_luminance_tex$Reducer <- rep("mk2 - texture", nrow(mk2_luminance_volA_tex_bound))
 
 mk2_hausdorff_giraffe <- importData("mk2-hausdorff-giraffe.csv")
-mk2_hausdorff_800 <- importData("mk2-hausdorff-800.csv")
 
+mk2_hausdorff_200 <- importData("mk2-hausdorff-200.csv")
+mk2_hausdorff_600 <- importData("mk2-hausdorff-600.csv")
+mk2_hausdorff_610 <- importData("mk2-hausdorff-610.csv")
+mk2_hausdorff_700 <- importData("mk2-hausdorff-700.csv")
+mk2_hausdorff_710 <- importData("mk2-hausdorff-710.csv")
+mk2_hausdorff_800 <- importData("mk2-hausdorff-800.csv")
+mk2_hausdorff_810 <- importData("mk2-hausdorff-810.csv")
+mk2_hausdorff_900 <- importData("mk2-hausdorff-900.csv")
+mk2_hausdorff_910 <- importData("mk2-hausdorff-910.csv")
+mk2_hausdorff_1000 <- importData("mk2-hausdorff-1000.csv")
+
+
+
+volume <- data.frame(Detail=factor(c(" base", " super", " high", " medium", " low"), 
+                                   c(" base", " super", " high", " medium", " low")))
+
+factor(levels=c(" base", " super", " high", " medium", " low"))
 comb <- create_header(colnames(data1))
+
 comb <- insert(comb, mk_distance, 1, 2)
 comb <- insert(comb, mk2_distance_tex, 1, 2)
 comb <- insert(comb, mk2_distance_tex_bound, 1, 2)
@@ -144,13 +162,37 @@ plot_tri_tex_error(comb)
 plot_tri_color_error(comb)
 plot_tri_geom_tex_error(comb)
 
-comb <- create_header(colnames(data1))
-comb <- insert(comb, mk2_hausdorff_800, 1, 5)
 plot_error(mk2_hausdorff_800, "GeoError")
 plot_error(mk2_hausdorff_800, "TexError")
 plot_error(mk2_hausdorff_800, "ColError")
+plot_error(mk2_hausdorff_800, "sqrt(ColError)")
 plot_error(mk2_hausdorff_800, "GeoError * ColError")
 plot_error(mk2_hausdorff_800, "GeoError + ColError")
+
+
+mk2_hausdorff_800 <- importData("mk2-hausdorff-800.csv")
+mk2_hausdorff_1000 <- importData("mk2-hausdorff-1000.csv")
+
+comb <- create_header(colnames(mk2_hausdorff_800))
+comb <- insert(comb, mk2_hausdorff_800, 1, 4)
+
+comb <- create_header(colnames(mk2_hausdorff_1000))
+comb <- insert(comb, mk2_hausdorff_1000, 1, 4)
+
+plot_error(comb, "GeoError")
+plot_error(comb, "TexError")
+plot_error(comb, "ColError")
+plot_error(comb, "sqrt(ColError)")
+plot_error(comb, "GeoError * ColError")
+plot_error(comb, "GeoError + ColError")
+
+plot_error_labeled(comb, "sqrt(GeoError)", "Triangles", "Rms geometric error")
+plot_error_labeled(comb, "sqrt(ColError)", "Triangles", "Rms color error")
+plot_error_labeled(comb, "sqrt(LumError)", "Triangles", "Rms luminance error")
+plot_error_labeled(comb, "sqrt(GeoError)*sqrt(ColError)", "Triangles", "Rms geometric * rms color error")
+plot_error_labeled(comb, "sqrt(GeoError)+sqrt(ColError)", "Triangles", "Rms geometric + rms color error")
+plot_error_labeled(comb, "GeoMaxError", "Triangles", "Max geometric error")
+plot_error_labeled(comb, "ColMaxError", "Triangles", "Max color error")
 
 ggplot(data_tex[as.character(data_tex$Detail) != " base",], aes(Triangles, Error, color=Model)) +
   geom_point() +
@@ -233,17 +275,54 @@ comb <- create_header(colnames(data1))
 comb <- insert(comb, mk2_hausdorff_vol_tex, 1, 1)
 comb <- insert(comb, mk2_hausdorff_vol_tex_bound, 1, 1)
 
+comb <- create_header(colnames(data1))
+comb <- insert(comb, mk2_luminance_tex, 1, 9)
+comb <- insert(comb, mk2_luminance_tex_bound, 1, 9)
+comb <- insert(comb, mk2_luminance_tex_vol, 1, 9)
+comb <- insert(comb, mk2_luminance_tex_vol_bound, 1, 9)
 
-df <- comb
-Summary <- df %>%
-  group_by(Reducer, Detail) %>%
-  summarise(Mean = mean(Error), SD = sd(Error))
+comb <- create_header(colnames(mk2_hausdorff_200))
+comb <- insert(comb, mk2_hausdorff_200, 1, 4)
+comb <- insert(comb, mk2_hausdorff_600, 1, 4)
+comb <- insert(comb, mk2_hausdorff_610, 1, 4)
+comb <- insert(comb, mk2_hausdorff_700, 1, 4)
+comb <- insert(comb, mk2_hausdorff_710, 1, 4)
+comb <- insert(comb, mk2_hausdorff_800, 1, 4)
+comb <- insert(comb, mk2_hausdorff_810, 1, 4)
+comb <- insert(comb, mk2_hausdorff_900, 1, 4)
+comb <- insert(comb, mk2_hausdorff_910, 1, 4)
 
 pd <- position_dodge(0.1)
 
-ggplot(Summary, aes(x=Detail, y=Mean, colour=Reducer, group=Reducer)) + 
-  geom_errorbar(aes(ymin=Mean-SD, ymax=Mean+SD), width=.1, position=pd) +
-  geom_line(position=pd) +
-  geom_point(position=pd)
+mk2_hausdorff_800 <- importData("mk2-hausdorff-800.csv")
 
+df <- mk2_hausdorff_800[as.character(mk2_hausdorff_800$Detail) != " base",]
+df <- mk2_hausdorff_800
+df <- comb[as.character(comb$Detail) != "base",]
+
+Summary <- df %>%
+  group_by(Reducer, Detail) %>%
+  summarise(Mean = mean(GeoError), SD = sd(GeoError))
+
+ggplot(Summary, aes(x=Detail, y=Mean, colour=Reducer, group=Reducer, shape=Reducer)) + 
+  geom_errorbar(aes(ymin=Mean-SD, ymax=Mean+SD), width=.3, position=pd) +
+  geom_line(position=pd) +
+  scale_shape_manual(values=c(19, 18, 17, 15)) +
+  geom_point(position=pd, size=3) +
+  ylab("Rms luminance error") +
+  scale_y_log10() +
+  annotation_logticks()
+
+ggplot(comb, aes(x=Detail, y=ColError, colour=Reducer, group=Reducer, shape=Model)) + 
+  scale_shape_manual(values=c(15, 16, 17, 0, 1, 2, 6, 7, 8)) +
+  geom_point(size=4) 
+
+rms <- aggregate(Error*Error ~ Detail+Reducer, df, sum) %>%
+  setNames(c("Detail", "Reducer", "Error")) %>%
+  aggregate(Error ~ Detail+Reducer, ., sqrt)
+
+ggplot(rms, aes(x=Detail, y=Error/9, colour=Reducer, group=Reducer, shape=Reducer)) + 
+  scale_shape_manual(values=c(19, 18, 17, 15)) +
+  geom_point(size=4) +
+  geom_line()
 
